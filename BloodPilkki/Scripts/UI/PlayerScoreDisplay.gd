6e029d53a0e4eaa090752fdc11ad87f447e86ce7
Niko Salakka <nsalakka@gmail.com>
2018-11-26 08:31:52 +0200

    stuff all over the place

---
+++ BloodPilkki/Scripts/UI/PlayerScoreDisplay.gd
@@ -0,0 +1,32 @@
+extends HSplitContainer
+
+var player = null
+var player_name = "Player X"
+var player_score = 0
+
+var _player_name_label = null
+var _player_score_label = null
+
+func _ready():
+	var level = get_tree().get_root().get_node("Root")
+	level.connect("score_added", self, "_on_score_added")
+	self._player_name_label = self.get_child(0)
+	self._player_score_label = self.get_child(1)
+	
+func init(player):
+	self.player = player
+	self.player_name = player.name
+	self.player_score = 0
+	self._update_ui(true)
+	
+func _update_ui(refresh_name=false):
+	if refresh_name:
+		self._player_name_label.set_text(self.player_name)
+	self._player_score_label.set_text("%s" % self.player_score)
+
+func _on_score_added(player, score):
+	if player != self.player:
+		return
+	
+	self.player_score += score
+	self._update_ui()
